fed925ab4817ecadd555145443cc6463b0cd1f13
drsanti <santi.inc.kmutt@gmail.cm>
2020-04-24 10:08:06 +0700

    update

--- examples/apps/demos/demo11_gauges_monitor/main.js
+++ examples/apps/demos/demo11_gauges_monitor/main.js
@@ -3,22 +3,40 @@ let Gauges = [];
 
 function init(link, ui) {
 
-    example1(link);
+    // example1(link);
+    example2(link);
 }
 
 function example1(link) {
 
     Gauges = createGauges();
 
-    let adcId = 0;
     setInterval( () => {
-        link.adcGet(adcId, (uid, val, err) => {
-            if(!err) {
-                UpdateGauge(uid, val);
-            }
-        });
-        adcId = (adcId + 1 ) % Gauges.length;
-    }, 250);
+
+        for(let i = 0; i<4; i++) {
+            link.adcGet(i, (uid, val, err) => {
+                if(!err) {
+                    UpdateGauge(uid, val);
+                    console.log(`adc${uid}: ${val}`);
+                }
+            });
+        }
+    }, 500);
+}
+
+function example2(link) {
+
+    // Gauges = createGauges();
+    // link.setAutoSampling({
+    //     enbAdcs: [true,  true,  true,  true],
+    //     enbPsws: [false, false, false, false],
+    //     enbLeds: [false, false, false, false],
+    //     interval: 500,
+    //     callback: (data) => {
+    //         console.log(`adcs: ${data.adc}`);
+    //         Gauges.map( (g, i) => UpdateGauge(i, data.adc[i]));
+    //     }
+    // });
 }
 
 function UpdateGauge(ch, val) {
@@ -43,12 +61,13 @@ function createGauges() {
 
 
 function main(ui) {
-    const link = new WSLink({
-        conCallback: (ws) => { },
-        datCallback: (ws, evt) => { },
-        ledCallback: (ch, sts) => {  },
-        pswCallback: (ch, sts) => { },
-        adcCallback: (ch, val, dif, dir) => { }
-    });
+    const link = new WSLink({nrsCallback: (msg) => {
+        console.log(msg)
+    }});
     init(link, ui);
+
+    link.adcSetAutoDetection(0, 100, 1000);
+    link.adcSetAutoDetection(1, 100, 1000);
+    link.adcSetAutoDetection(2, 100, 1000);
+    link.adcSetAutoDetection(3, 100, 1000);
 }
